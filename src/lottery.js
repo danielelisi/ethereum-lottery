e6fba8b329f816c39d0bbdeefa6590f83c7cba89
Daniele Lisi <danielelisi978@gmail.com>
2018-03-25 18:14:24 -0700

    :sparkles:  front end interactions with the contract

--- src/lottery.js
+++ src/lottery.js
@@ -1,7 +1,7 @@
 import web3 from './web3';
-import Lottery from '../ethereum-contract/build/Lottery.json';
+import lotteryInterface from './lotteryInterface.json';
 
-const address = process.env.CONTRACT_ADDRESS;
-const abi = Lottery.interface;
+const address = '0x69CF024821C9731AF5088ab6bCa036B5b5E1A1D4';
+const abi = JSON.parse(lotteryInterface);
 
 export default new web3.eth.Contract(abi, address);
--- src/web3.js
+++ src/web3.js
@@ -1,6 +1,5 @@
 import Web3 from 'web3';
 
-// 
-const Web3 = new Web3(window.web3.currentProvider);
+const web3 = new Web3(window.web3.currentProvider);
 
 export default web3;
