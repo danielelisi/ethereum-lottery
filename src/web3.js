df75b36e76eac7e2b041499f08643352fe009038
Daniele Lisi <danielelisi978@gmail.com>
2018-03-25 18:35:24 -0700

    :bug: updated src/web3.js to display message if user is not running METAMASK

--- src/web3.js
+++ src/web3.js
@@ -1,5 +1,19 @@
 import Web3 from 'web3';
 
-const web3 = new Web3(window.web3.currentProvider);
+let web3;
+
+if (typeof window !== 'undefined' && typeof window.web3 !== 'undefined')  {
+    //  We are in the browser and metamask is running.
+    web3 = new Web3(window.web3.currentProvider);
+} else {
+    //  we are on the server *OR* the user is not running metamask.
+    const provider = new Web3.providers.HttpProvider(
+        'https://rinkeby.infura.io/'
+    );
+
+    web3 = new Web3(provider);
+
+    web3.message = 'You need METAMASK browser extension running in order to interact with the contract.';
+}
 
 export default web3;
